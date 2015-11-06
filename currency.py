ba774a2e5ee48b3b092a1ee17783d339b3835a1d
Ravali <ravali.pasupuleti@my.jcu.edu.au>
2015-11-06 19:11:43 +1000

    Update currency.py

--- currency.py
+++ currency.py
@@ -4,26 +4,26 @@ import web_utility
 #Function 1 convert
 def convert(amount, home_currency_code, location_currency_code):
 
-    urlString = "https://www.google.com/finance/converter?a="+str(amount)+"&from="+home_currency_code+"&to="+location_currency_code
-    print(urlString)
+    url_string = "https://www.google.com/finance/converter?a="+str(amount)+"&from="+home_currency_code+"&to="+location_currency_code
+    print(url_string)
     result = web_utility.load_page(urlString)
 
     try :
-        indexofMatchedData = result.index('<span class=bld>')
-        newmatchedString =  result[indexofMatchedData:]
+        index_of_matched_data = result.index('<span class=bld>')
+        newmatched_string =  result[index_of_matched_data:]
         # This will return me this text <span class=bld>0.7426 AUD</span>\n<input type= ....>
         # Next I will take the data till </span> so as to get the currency
-        newmatchedString = newmatchedString[newmatchedString.index('<span class=bld>')+16:newmatchedString.index('</span>')]
+        newmatched_string = newmatched_string[newmatched_string.index('<span class=bld>')+16:newmatched_string.index('</span>')]
         # index of span class=bld + 16. 16 is length of <span class=bld> and hence start index to get value should be after +16.
         #output 10 USD
         # we just need 10
-        return newmatchedString[0:newmatchedString.index(" ")]
+        return newmatched_string[0:newmatched_string.index(" ")]
     except ValueError :
         return "-1"
 
 
 #Function 2 getDetails
-def getDetails(countryName):
+def get_details(country_name):
     text_file = open("currency details.txt", "r")
     # fetching a list of values
     lines = text_file.readlines()
@@ -31,17 +31,15 @@ def getDetails(countryName):
 
     checkFlag = 0
     tup = ()
-    for eachLine in lines:
+    for each_line in lines:
 
-        eachLineTuppleSplit = eachLine.split(",")
+        each_line_tuple_split = each_line.split(",")
         # For loop and eachLine is the tupple for each currency
-        if countryName.upper() == eachLineTuppleSplit[0].upper():
+        if country_name.upper() == each_line_tuple_split[0].upper():
              print("Country Name Found")
              # In the next line we make a tupple and remove whitespace in the front for tupple index(0) and remove new line for index(2) so that we get proper output
-             return (eachLineTuppleSplit[0].replace('\ufeff', ''),eachLineTuppleSplit[1],eachLineTuppleSplit[2].replace('\n',''))
+             return (each_line_tuple_split[0].replace('\ufeff', ''), each_line_tuple_split[1], each_line_tuple_split[2].replace('\n',''))
              #Output ('Japan', 'JPY', '¥')
-
-
     return ()
 
 
