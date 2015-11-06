514c034db0876d562023f6ed5f29c72a0d9812d7
Ravali <ravali.pasupuleti@my.jcu.edu.au>
2015-11-06 18:36:47 +1000

    Update trip.py

--- trip.py
+++ trip.py
@@ -4,27 +4,26 @@ class Error(Exception):
         self.msg = arg
 
 
-
 class Country:
 
     name = ""
     currency_code = ""
     currency_symbol = ""
 
-    def __init__(self, name , currency_code , currency_symbol):
+    def __init__(self, name, currency_code, currency_symbol):
         self.name = name
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
 
 
-    def formatCurrency(self,amount):
-        roundedAmount = amount/100
-        roundededCent = round(roundedAmount)
-        roundedCentAmount = roundededCent*100
-        return self.currency_symbol+""+str(roundedCentAmount)
+    def format_currency(self, amount):
+        rounded_amount = amount/100
+        rounded_cent = round(rounded_amount)
+        rounded_cent_amount = rounded_cent*100
+        return self.currency_symbol + "" + str(rounded_cent_amount)
 
     def __str__(self):
-        return  self.name+" "+self.currency_code +" "+self.currency_symbol
+        return  self.name + " " + self.currency_code + " " + self.currency_symbol
 
 
 
@@ -35,90 +34,90 @@ class Details:
     def __init__(self, locations):
        self.locations = locations
 
-    def add(self,country_name,start_date, end_date):
+    def add(self, country_name, start_date, end_date):
 
-            tripDetail = "\n"+country_name+","+start_date+","+end_date
+            trip_detail = "\n" + country_name + "," + start_date + "," + end_date
             start_date_parts = start_date.split('/')
             end_date_parts = end_date.split('/')
 
             if len(start_date_parts) != 3:
-                raise Error("Invalid Start Date Error"+tripDetail)
+                raise Error("Invalid Start Date Error" + trip_detail)
             elif len(start_date_parts[0]) != 4:
-                raise Error("Invalid Start Date Error"+tripDetail)
+                raise Error("Invalid Start Date Error" + trip_detail)
             elif len(start_date_parts[1]) != 2:
-                raise Error("Invalid Start Date Error"+tripDetail)
+                raise Error("Invalid Start Date Error" + trip_detail)
             elif len(start_date_parts[2]) != 2:
-                raise Error("Invalid Start Date Error"+tripDetail)
+                raise Error("Invalid Start Date Error" + trip_detail)
 
             try:
-                startYear = int(start_date_parts[0])
-                startMonth = int(start_date_parts[1])
-                startDay = int(start_date_parts[2])
+                start_year = int(start_date_parts[0])
+                start_month = int(start_date_parts[1])
+                start_day = int(start_date_parts[2])
 
             except ValueError:
-                raise Error("Invalid Start Date Error"+tripDetail)
+                raise Error("Invalid Start Date Error" + trip_detail)
 
             if len(end_date_parts) != 3:
-                raise Error("Invalid End Date Error"+tripDetail)
+                raise Error("Invalid End Date Error" + trip_detail)
             elif len(end_date_parts[0]) != 4:
-                raise Error("Invalid End Date Error"+tripDetail)
+                raise Error("Invalid End Date Error" + trip_detail)
             elif len(end_date_parts[1]) != 2:
-                raise Error("Invalid End Date Error"+tripDetail)
+                raise Error("Invalid End Date Error"+ trip_detail)
             elif len(end_date_parts[2]) != 2:
-                raise Error("Invalid End Date Error"+tripDetail)
+                raise Error("Invalid End Date Error" + trip_detail)
 
             try:
-                endYear = int(end_date_parts[0])
-                endMonth = int(end_date_parts[1])
-                endDay = int(end_date_parts[2])
+                end_year = int(end_date_parts[0])
+                end_month = int(end_date_parts[1])
+                end_day = int(end_date_parts[2])
 
             except ValueError:
-                raise Error("Invalid End Date Error"+tripDetail)
+                raise Error("Invalid End Date Error" + trip_detail)
 
             if int(start_date_parts[0]) > int(end_date_parts[0]):
-                raise Error("Start Date Greater than End Date"+tripDetail)
+                raise Error("Start Date Greater than End Date"+ trip_detail)
             elif int(start_date_parts[1]) > int(end_date_parts[1]):
-                raise Error("Start Date Greater than End Date"+tripDetail)
+                raise Error("Start Date Greater than End Date"+ trip_detail)
             elif int(start_date_parts[2]) > int(end_date_parts[2]):
-                raise Error("Start Date Greater than End Date"+tripDetail)
+                raise Error("Start Date Greater than End Date"+ trip_detail)
 
-            listLocs = self.locations
-            if len(listLocs) != 0:
-                for eachTrip in listLocs:
-                    # print(eachTrip)
+            list_of_locations = self.locations
+            if len(list_of_locations) != 0:
+                for each_trip in list_of_locationss:
+                    # print(each_trip)
 
-                    eachTripStartDate = eachTrip[1]
-                    eachTripEndDate = eachTrip[2]
+                    each_trip_start_date = each_trip[1]
+                    each_trip_end_date = each_trip[2]
 
-                    eachTripStartDateArray = eachTripStartDate.split('/')
-                    eachTripEndDateArray = eachTripEndDate.split('/')
+                    each_trip_start_date_array = each_trip_start_date.split('/')
+                    each_trip_end_date_array = each_trip_end_date.split('/')
 
-                    eachTripStartDateTupple = (int(eachTripStartDateArray[0]),int(eachTripStartDateArray[1]),int(eachTripStartDateArray[2]))
-                    eachTripEndDateTupple = (int(eachTripEndDateArray[0]),int(eachTripEndDateArray[1]),int(eachTripEndDateArray[2]))
+                    each_trip_start_date_tuple = (int(each_trip_start_date_array[0]), int(each_trip_start_date_array[1]), int(each_trip_start_date_array[2]))
+                    each_trip_end_date_uple = (int(each_trip_end_date_array[0]), int(each_trip_end_date_array[1]), int(each_trip_end_date_array[2]))
 
-                    scheduledTripStartDateTupple = (startYear,startMonth,startDay)
-                    scheduledTripEndDateTupple = (endYear,endMonth,endDay)
-
-
-                    if eachTripStartDateTupple <= scheduledTripStartDateTupple < eachTripEndDateTupple:
-                        raise Error(" Date alreday present in Trip Schedule. \n Trip Details :  "+str(eachTrip))
-
-                    if eachTripStartDateTupple < scheduledTripEndDateTupple <= eachTripEndDateTupple:
-                        raise Error("Date alreday present in Trip Schedule. \n Trip Details :  "+str(eachTrip))
-
-                    if scheduledTripStartDateTupple < eachTripStartDateTupple  and scheduledTripEndDateTupple > eachTripEndDateTupple:
-                        raise Error("Date alreday present in Trip Schedule. \n Trip Details :  "+str(eachTrip))
+                    scheduled_trip_start_date_tuple = (start_year, start_month, start_day)
+                    scheduled_trip_end_date_tuple = (end_year, end_month, end_day)
 
 
+                    if each_trip_start_date_tuple <= scheduled_trip_start_date_tuple < each_trip_end_date_tuple:
+                        raise Error(" Date alreday present in Trip Schedule. \n Trip Details :  " + str(each_trip))
 
-                listLocs.append((country_name,start_date, end_date))
+                    if each_trip_start_date_tuple < scheduled_trip_end_date_tuple <= each_trip_end_date_tuple:
+                        raise Error("Date alreday present in Trip Schedule. \n Trip Details :  " + str(each_trip))
+
+                    if scheduled_trip_start_date_tupple < each_trip_start_date_tuple  and scheduled_trip_end_date_tuple > each_trip_end_date_tuple:
+                        raise Error("Date alreday present in Trip Schedule. \n Trip Details :  " + str(each_trip))
+
+
+
+                list_of_locations.append((country_name, start_date, end_date))
             else:
-                listLocs.append((country_name,start_date, end_date))
+                list_of_locations.append((country_name, start_date, end_date))
 
 
-            print("List of Locations : "+ str(listLocs))
+            print("List of Locations : " + str(list_of_locations))
 
-    def current_country(self,date_string):
+    def current_country(self, date_string):
 
         try:
             start_date_parts = date_string.split('/')
@@ -132,46 +131,43 @@ class Details:
                 raise Error("Invalid Input Date Error")
 
             try:
-                startYear = int(start_date_parts[0])
-                startMonth = int(start_date_parts[1])
-                startDay = int(start_date_parts[2])
-                date_string_tupple = (startYear,startMonth,startDay)
+                start_year = int(start_date_parts[0])
+                start_month = int(start_date_parts[1])
+                start_day = int(start_date_parts[2])
+                date_string_tuple = (start_year, start_month, start_day)
 
             except ValueError:
                 raise Error("Invalid Input Date Error")
 
-            listLocs = self.locations
+            list_of_locations = self.locations
 
-            if len(listLocs) != 0:
+            if len(list_of_locations) != 0:
 
-                for eachTrip in listLocs:
+                for each_trip in list_of_locations:
 
-                    # print(eachTrip)
-                    eachTripStartDate = eachTrip[1]
-                    eachTripEndDate = eachTrip[2]
+                    # print(each_trip)
+                    each_trip_start_date = each_trip[1]
+                    each_trip_end_date = each_trip[2]
 
-                    startDateArray = eachTripStartDate.split('/')
-                    endDateArray = eachTripEndDate.split('/')
+                    start_date_array = each_trip_start_date.split('/')
+                    end_date_array = each_trip_end_date.split('/')
 
-                    startDateTupple = (int(startDateArray[0]),int(startDateArray[1]),int(startDateArray[2]))
-                    endDateTupple = (int(endDateArray[0]),int(endDateArray[1]),int(endDateArray[2]))
+                    start_date_tuple = (int(start_date_array[0]), int(start_date_array[1]), int(start_date_Array[2]))
+                    end_date_tuple = (int(end_date_array[0]), int(end_date_array[1]), int(end_date_array[2]))
 
-                    if startDateTupple < date_string_tupple < endDateTupple:
-                        print("Country on date : "+date_string+" - " + eachTrip[0])
-                        return  eachTrip[0]
+                    if start_date_tuple < date_string_tuple < end_date_tuple:
+                        print("Country on date : " + date_string + " - " + each_trip[0])
+                        return  each_trip[0]
                         break
-                        #Source : http://stackoverflow.com/questions/5464410/how-to-tell-if-a-date-is-between-two-other-dates-in-python
 
-                raise  Error("Error : No Country Found on Date :"+date_string)
-            raise  Error("Error : No Country Found on Date :"+date_string)
+                raise  Error("Error : No Country Found on Date :" + date_string)
+            raise  Error("Error : No Country Found on Date :" + date_string)
 
-        except Error as customTripError:
-            print(customTripError.msg)
-
+        except Error as custom_trip_error:
+            print(custom_trip_error.msg)
 
 
-
-    def isEmpty(self):
+    def is_empty(self):
         if len(self.locations) > 0:
             return "Is Not Empty"
         else:
